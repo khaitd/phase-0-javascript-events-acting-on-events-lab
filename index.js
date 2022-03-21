8824b7ccd4cd0809a26b94cf1947aef5e8f2d4ca
Dmitriy <khaitdmitriy@gmail.com>
2022-03-21 17:55:46 -0400

    commit tree cd3909117d7a2ebcbd45a07fbc3b96ee5ae760f9

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+
+// function moveDodgerLeft(){
+
+//     document.addEventListener('keydown', (e) => {
+       
+//         const position = dodger.style.left.replace("px", "");
+//         const newPosition = parseInt(position, 10);
+//          if (left > 0 )   {
+//         dodger.style.left = `${newPosition-1}px`
+//          }
+        
+//     })
+// }    
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+moveDodgerLeft()
+
+
+function moveDodgerRight(){
+    document.addEventListener('keydown', (e) => {
+        if (e.key == "ArrowRight"){
+            const position = dodger.style.left.replace("px", "");
+            const newPosition = parseInt(position, 10);
+
+            dodger.style.left = `${newPosition+1}px`
+        }
+    })
+}    
+
+moveDodgerRight()
